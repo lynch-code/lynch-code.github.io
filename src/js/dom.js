d2b583e1a527960379bc332f5bae4eeb80e00abd
Lynch <78087628+lynch-code@users.noreply.github.com>
2022-11-15 18:51:02 +0700

    Add files via upload

---
+++ src/js/dom.js
@@ -0,0 +1,10 @@
+let link = document.getElementById('link')
+link.setAttribute("href", "https://fian014.site")
+
+function createLink() {
+    const cL = document.getElementById('create')
+    const link = document.getElementById('test')
+
+    link.innerHTML = "Link"
+    document.body.appendChild()
+}
