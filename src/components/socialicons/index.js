fc5ec1107701e72f1f0e438c1d1579552498ac23
Lynch <78087628+lynch-code@users.noreply.github.com>
2022-12-20 15:03:55 +0700

    Add files via upload

---
+++ src/components/socialicons/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./style.css";
+import {
+  FaGithub,
+  FaTwitter,
+  FaPinterestP,
+  FaDev,
+  FaFacebookF,
+  FaInstagram,
+} from "react-icons/fa";
+import { socialprofils } from "../../content_option";
+
+export const Socialicons = (params) => {
+  return (
+    <div className="stick_follow_icon">
+      <ul>
+        {socialprofils.twitter && (
+          <li>
+            <a href={socialprofils.twitter}>
+              <FaTwitter />
+            </a>
+          </li>
+        )}
+        {socialprofils.github && (
+          <li>
+            <a href={socialprofils.github}>
+              <FaGithub />
+            </a>
+          </li>
+        )}
+        {socialprofils.dev && (
+          <li>
+            <a href={socialprofils.dev}>
+              <FaDev />
+            </a>
+          </li>
+        )}
+        {socialprofils.instagram && (
+          <li>
+            <a href={socialprofils.instagram}>
+              <FaInstagram />
+            </a>
+          </li>
+        )}
+      </ul>
+      <p>Follow Me</p>
+    </div>
+  );
+};
---
+++ src/content_option.js
@@ -0,0 +1,136 @@
+const logotext = "Fian014";
+const meta = {
+    title: "Fian014",
+    description: "Fian014 Doe data scientist _ Front-End Developer,currently working in Indonesia",
+};
+
+const introdata = {
+    title: "I’m Fian014",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I like the simple design of the website",
+    },
+    description: "Hopefully this website will be a source of inspiration and useful information for you and other visitors. Happy learning and keep growing as a great front-end developer!",
+    your_img_url: "https://i.pinimg.com/564x/bf/18/7f/bf187ffa1af39013eacbd3e526414ca5.jpg",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "A student who is studying in the field of Front-End Developer. Sometimes they also make WhatsApp Bot technology, but only the simple ones. Sometimes also make a simple GFX Design to calm the mind.",
+};
+const worktimeline = [{
+        jobtitle: "Learn Front-End Framework",
+        where: "Reactjs",
+        date: "2022",
+    },
+    {
+        jobtitle: "Try UI UX Design",
+        where: "Figma",
+        date: "2021",
+    },
+    {
+        jobtitle: "Learn Basic Web Developer",
+        where: "HTML, CSS, JS",
+        date: "2021",
+    },
+];
+
+const skills = [{
+        name: "React",
+        value: 40,
+    },
+    {
+        name: "CSS",
+        value: 80,
+    },
+    {
+        name: "Javascript",
+        value: 75,
+    },
+    {
+        name: "HTML",
+        value: 95,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Both UI and UX design are important for creating products that are easy to use and provide a good experience for users. While UI design focuses on the visual and interactive elements of the product, UX design focuses on the overall experience of using the product.",
+    },
+    {
+        title: "Digital Marketing",
+        description: "Digital marketing is the process of promoting and selling products or services using digital technologies, such as the internet, social media, and mobile apps. It involves using a variety of online channels and tactics to reach and engage potential customers, and to convert them into customers.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.pinimg.com/564x/4e/8d/9e/4e8d9eb8e7ec4beb41f5458b4a5ab4b5.jpg",
+        desctiption: "First Try Figma App",
+        link: "https://pin.it/2hZFEkJ",
+    },
+    {
+        img: "https://i.pinimg.com/564x/fc/df/38/fcdf38a4ecb5802ff3402f557f5e8081.jpg",
+        desctiption: "Try to make UI Application.",
+        link: "https://pin.it/2hZFEkJ",
+    },
+    {
+        img: "https://i.pinimg.com/564x/49/c1/ad/49c1ad8f16b184243dab9e9233faf354.jpg",
+        desctiption: "Try to make bot WhatsApp Cover.",
+        link: "https://pin.it/2hZFEkJ",
+    },
+    {
+        img: "https://i.pinimg.com/564x/61/c3/20/61c3209eaa07f8c7f9583e4c796887fd.jpg",
+        desctiption: "Landing page search",
+        link: "https://pecorine-search.netlify.app/",
+    },
+    {
+        img: "https://i.pinimg.com/564x/7b/7a/e2/7b7ae271abe42733b7785eef03ccae1b.jpg",
+        desctiption: "Simple Design Product.",
+        link: "https://pin.it/6Rlm8Ld",
+    },
+    {
+        img: "https://i.pinimg.com/564x/8a/61/f7/8a61f7d4db39d8e73d4b3f01661c7f7e.jpg",
+        desctiption: "Design Product PC and Android",
+        link: "https://pin.it/3OJQYOW",
+    },
+
+    {
+        img: "https://i.pinimg.com/564x/b8/85/b1/b885b1d4ac62c354f302215d890d9564.jpg",
+        desctiption: "Simple Website Clock",
+        link: "https://fiancuy.my.id",
+    },
+    {
+        img: "https://i.pinimg.com/564x/5f/07/ce/5f07cea5d2428bd8baf54d50d97bc482.jpg",
+        desctiption: "First Website With Bootstraps 4",
+        link: "https://fian014.site",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "fncode17@gmail.com",
+    YOUR_FONE: "(+62) 85161600536",
+    description: "If you have any questions, you can contact me!",
+    YOUR_SERVICE_ID: "service_knyf0ud",
+    YOUR_TEMPLATE_ID: "template_kqcpwqb",
+    YOUR_USER_ID: "uvb8hSP_ay0EqsOI7",
+};
+
+const socialprofils = {
+    github: "https://github.com/FianEunoia",
+    dev: "https://dev.to/fianeunoia",
+    twitter: "https://twitter.com/fiannnn14",
+    instagram: "https://instagram.com/fiannn14_",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
